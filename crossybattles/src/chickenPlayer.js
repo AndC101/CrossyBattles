500b270dbf1bcd1d36cbabea56268b56f12cddc8
Andrew Chen <andrew07.chen@gmail.com>
2025-04-05 13:54:06 -0400

    update

---
+++ crossybattles/src/chickenPlayer.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Chicken from './sprites'
+
+// everything is gonna move backward, the chicken stays in place
+
+function chickenPlayer() {
+    // const [map, setMap] = useState([]); 
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default chickenPlayer
