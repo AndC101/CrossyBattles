221da6f602780d526b79576b817615bf8cd2e3f2
notjing <30694907+notjing@users.noreply.github.com>
2025-04-05 10:18:59 -0400

    mainmenu

--- crossybattles/src/MainMenu.js
+++ crossybattles/src/MainMenu.js
@@ -4,6 +4,20 @@ import './App.css';
 function MainMenu() {
   return (
     <div className="App">
+      <div className="title">
+        Crossy Battles
+      </div>
+
+      <div className="buttons">
+        <div className="create-game">
+          <button className="create-game-button">Create Game</button>
+        </div>
+
+        <div className="join-game">
+          <button className="join-game-button">Join Game</button>
+        </div>
+        
+      </div>
       
       
     </div>
